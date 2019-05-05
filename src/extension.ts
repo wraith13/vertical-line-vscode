db9503af25d96d6a9173a4ae2d74b5f77445b84c
道化師 <wraith13@hotmail.co.jp>
2019-05-05 22:40:25 +0900

    🛠 暫定的な現状の実装のコミット

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,118 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+/*
+import localeEn from "../package.nls.json";
+import localeJa from "../package.nls.ja.json";
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vertical-line-vscode" is now active!');
+interface LocaleEntry
+{
+    [key : string] : string;
+}
+const localeTableKey = <string>JSON.parse(<string>process.env.VSCODE_NLS_CONFIG).locale;
+const localeTable = Object.assign(localeEn, ((<{[key : string] : LocaleEntry}>{
+    ja : localeJa
+})[localeTableKey] || { }));
+const localeString = (key : string) : string => localeTable[key] || key;
+*/
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vertical-line.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+export module VerticalLine
+{
+    //let pass_through;
+    
+    const applicationKey = "vertical-line";
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello Vertical Line!');
-    });
+    const getConfiguration = <type>(key? : string, section : string = applicationKey) : type =>
+    {
+        const configuration = vscode.workspace.getConfiguration(section);
+        return key ?
+            configuration[key] :
+            configuration;
+    };
+    const setEnabled = (enabled : boolean) : void => { vscode.workspace.getConfiguration().update("enabled", enabled, true); };
 
-    context.subscriptions.push(disposable);
+    let color : string = "dummy";
+    let enabled : boolean = false;
+
+    let decorator : vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({});
+
+    export function initialize(context : vscode.ExtensionContext): void
+    {
+        context.subscriptions.push
+        (
+            //  コマンドの登録
+            vscode.commands.registerCommand(`${applicationKey}.show`, show),
+            vscode.commands.registerCommand(`${applicationKey}.hide`, hide),
+
+            //  イベントリスナーの登録
+            vscode.workspace.onDidChangeConfiguration(() => onDidChangeConfiguration()),
+            vscode.window.onDidChangeTextEditorSelection(() => update()),
+            vscode.window.onDidChangeActiveTextEditor(() => update())
+        );
+
+        onDidChangeConfiguration();
+    }
+
+    export const show = () : void => setEnabled(true);
+    export const hide = () : void => setEnabled(false);
+
+    const calcWidth = (text : string) : number =>
+    {
+        return text.length;
+    };
+    const substrByWidth = (text : string, width : number) : string | null =>
+    {
+        return text.length < width ? null: text.substr(0, width);
+    } ;
+
+    export function update() : void
+    {
+        const editor = vscode.window.activeTextEditor;
+        if (editor)
+        {
+            var decorations = [];
+            if (editor.selection.isEmpty && enabled)
+            {
+                const width = calcWidth(editor.document.lineAt(editor.selection.start.line).text.substr(0, editor.selection.start.character));
+                for (var i = 0; i < editor.document.lineCount; i++)
+                {
+                    //if (editor.selection.end.character <= editor.document.lineAt(i).text.replace(/(^ +).*$/, "$1").length)
+                    var sub = substrByWidth(editor.document.lineAt(i).text, width);
+                    if (null !== sub)
+                    {
+                        decorations.push(new vscode.Range(i, sub.length,i, sub.length));
+                    }
+                }
+            }
+            editor.setDecorations(decorator, decorations);
+    }
+    }
+
+    export const onDidChangeConfiguration = () =>
+    {
+        vscode.window.visibleTextEditors.forEach(editor => editor.setDecorations(decorator, []));
+
+        color = getConfiguration<string>("color");
+        enabled =  getConfiguration<boolean>("enabled");
+        decorator.dispose();
+        decorator = vscode.window.createTextEditorDecorationType
+        (
+            {
+                borderColor: color,
+                borderStyle: 'none none none solid',
+                borderWidth: '1px'
+            }
+        );
+
+        update();
+    };
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+export function activate(context: vscode.ExtensionContext) : void
+{
+    VerticalLine.initialize(context);
+}
+
+export function deactivate() : void
+{
 }
